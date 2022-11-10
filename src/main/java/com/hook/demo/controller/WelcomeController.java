2c0961776e9755605ad16c827dbe3947e344d6f2
Venkata <venkatasrikrishna.chintalapati@cgi.com>
2022-11-10 15:58:22 -0500

    hook-bolt

---
+++ src/main/java/com/hook/demo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.hook.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Welcome to Hook Project";
+    }
+}
